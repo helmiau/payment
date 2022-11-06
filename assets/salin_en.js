0562b8dc75a66994859daf3a35244b66932741dc
Helmi Amirudin <helmilaw@gmail.com>
2022-11-07 00:44:05 +0700

    updated clipboard js

--- assets/salin_en.js
+++
@@ -1,8 +0,0 @@
-function SalinAku(TextToCopy) {
-	var TempText = document.createElement("input");
-	TempText.value = TextToCopy;
-	
-	prompt("Account name: Helmi Amirudin, Please copy information below manually", TempText.value);
-	/* Notifikasi */
-	/* alert("Teks disalin: " + TempText.value); */
-}
--- assets/salin_id.js
+++
@@ -1,9 +0,0 @@
-
-function SalinAku(TextToCopy) {
-	var TempText = document.createElement("input");
-	TempText.value = TextToCopy;
-	
-	prompt("Akun atas nama Helmi Amirudin, silahkan salin informasi dibawah ini", TempText.value);
-	/* Notifikasi */
-	/* alert("Teks disalin: " + TempText.value); */
-}
